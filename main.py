f35a76899fbce5efabcc8b58ae135fb81ef26161
Fernando Amaral <fdo_amaral@outlook.com>
2025-01-17 00:15:56 +0000

    Python skills test printing and calculator

---
+++ main.py
@@ -0,0 +1,32 @@
+
+print("Hello World")
+print ("this is a test ")
+print ("testing my python skills")
+
+x = 10
+y = 20
+my_name = "Fernando"
+
+sum = x + y
+
+print ("the result of the sum is ", sum)
+
+div = y / x
+
+print ("the result of the division is ", div)
+
+sub = y - x
+
+print ("the resul of the subtraction is ", sub)
+
+mul = x * y
+
+print ("the result of the multiplication is ", mul)
+
+
+if my_name == "Fernando":
+    print("Hello Fernando")
+
+if my_name == "Fernando":
+    print (x)
+
