49ac5926155224b91537f6f097f7a64f5c0aa752
Fernando Amaral <fdo_amaral@outlook.com>
2025-01-20 14:18:04 +0000

    Python calculator

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+# Calculator
+
+# Function defined as Calculator to perform the calculations
+def Calculator():
+
+    # Calculator introduction
+    print ("Welcome to My Python calculator project.")
+
+    # take the input from the user
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+
+    # Display calculation option
+    print ("Choose an operation:")
+    print ("1  - addition")
+    print ("2  - subtraction")
+    print ("3  - multiplication")
+    print ("4  - division")
+
+Calculator()
