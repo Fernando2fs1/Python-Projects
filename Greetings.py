fa402f927972f722a4f199aec23d5e9a6ca246d1
Fernando Amaral <fdo_amaral@outlook.com>
2025-01-20 15:22:31 +0000

    Greeting Program

---
+++ Greetings.py
@@ -0,0 +1,9 @@
+# Simple Greeting program
+
+# Function to greet the user
+def greetings():
+    
+    user_name = input("Enter your name: ")
+    print (f"Hello, {user_name}! Welcome to my python progress!")
+
+greetings()
